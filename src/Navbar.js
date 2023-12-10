2135c4b7eb3d5d596de16cdcea3342333d7b2b9c
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2023-12-10 14:06:54 +0800

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <div className="text-xl font-bold">Book Search App</div>
+        <div className="text-xl font-bold">Book Search App ~ Haikal's Project</div>
       </div>
     </nav>
   );
